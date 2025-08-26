6e89e76e131804859fa94fc212cdf3653d102121
Francis Addo <francis.t.addo@gmail.com>
2025-08-26 23:31:37 +0100

    #1) refactor dtos

--- TG.CarParkEsher/TG.CarParkEsher.Booking/Models/DTOs/Request/EsherCarParkRegistrationRequestDto.cs
+++ TG.CarParkEsher/TG.CarParkEsher.Booking/Models/DTOs/Request/EsherCarParkRegistrationRequestDto.cs
@@ -5,6 +5,7 @@
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string  EmplyeeId { get; set; }
+        public required List<string> VehicleType { get; set; }
         public required string Password { get; set; }
         public required string ConfirmPassword { get; set; }
     }
