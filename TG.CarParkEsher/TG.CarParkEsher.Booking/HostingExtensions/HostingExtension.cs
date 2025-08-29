97103767e4f89983d9466bfa4daf5a1f25c68f7a
Francis Addo <francis.t.addo@gmail.com>
2025-08-29 12:09:03 +0100

    #1) UPDATES

--- TG.CarParkEsher/TG.CarParkEsher.Booking/HostingExtensions/HostingExtension.cs
+++ TG.CarParkEsher/TG.CarParkEsher.Booking/HostingExtensions/HostingExtension.cs
@@ -73,11 +73,13 @@ namespace TG.CarParkEsher.Booking
 
             // Configure the HTTP request pipeline.
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
