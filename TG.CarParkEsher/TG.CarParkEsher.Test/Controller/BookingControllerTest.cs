62adae90efd08007d6964a45c025fcd399def284
Francis Addo <francis.t.addo@gmail.com>
2025-08-30 19:43:45 +0100

    #1) add unit test

---
+++ TG.CarParkEsher/TG.CarParkEsher.Test/Controller/BookingControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using TG.CarParkEsher.Booking;
+using TG.CarParkEsher.Booking.Controllers;
+
+namespace TG.CarParkEsher.Test.Controller
+{
+    public class BookingControllerTest
+    {
+        private readonly IBookingService _bookingService;
+        private readonly ILogger<BookingController> _logger;
+
+        public BookingControllerTest()
+        {
+            _bookingService = A.Fake<IBookingService>();
+            _logger = A.Fake<ILogger<BookingController>>();
+        }
+        [Fact]
+        public void BookingController_CreateBookingAsync_Returns()
+        {
+            // Arrange
+            var booking = A.Fake<EsherCarParkBookingResponseDto>();
+            var controller = new BookingController(_logger, _bookingService);
+
+
+            //Act
+            var result = controller.CreateBookingAsync(A.Fake<EsherCarParkBookingRequestDto>(), CancellationToken.None);
+            //Assert
+
+        }
+    }
+}
